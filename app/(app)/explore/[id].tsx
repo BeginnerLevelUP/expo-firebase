3f33a43b3ef8d6a2f1afd77727afdd14df01657f
Damion Morgan <only4school5680@gmail.com>
2024-07-29 20:14:58 -0400

    need to add width and height for react native image to work

--- app/(app)/explore/[id].tsx
+++ app/(app)/explore/[id].tsx
@@ -1,9 +1,12 @@
 
-import { View,ScrollView,Text } from 'react-native';
-import { Button,Card,} from 'react-native-paper';
+import { View,ScrollView,Text,StyleSheet,Image} from 'react-native';
 import React,{useEffect, useState} from "react"
+import { Button } from 'react-native-paper';
 import { useLocalSearchParams } from 'expo-router';
 import Meal from '@/utils/interface/meal';
+
+const blurhash =
+  '|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[';
 
 const SingleMeal = () => {
      const { id,data } = useLocalSearchParams();
@@ -18,14 +21,15 @@ const SingleMeal = () => {
             {meal?
             (
                   <View className='my-4' key={meal?.idMeal}>
-                      <Card>
-                        <Card.Title title={meal?.strMeal} subtitle={meal?.strArea} />
-                        <Card.Cover source={{ uri: `${meal?.strMealThumb}` }} />
-                        <Card.Actions>
-                          <Button>Add ddddd To Plan</Button>
+                          <Text>{meal.strMeal}</Text>
+                          <Text>{meal.strArea}</Text>
+                          <Button>Add To Plan</Button>
                           <Button>Save To Favorites</Button>
-                        </Card.Actions>
-                      </Card>
+                          <Image
+                            className='w-1/2 h-full'
+                            source={{ uri: meal.strMealThumb }}
+                            resizeMode="cover" // or "contain", "stretch"
+                          />
                   </View>
             ):(
                 <Text>Error Fetching Meal</Text>
@@ -37,3 +41,4 @@ const SingleMeal = () => {
 }
 
 export default SingleMeal
+
