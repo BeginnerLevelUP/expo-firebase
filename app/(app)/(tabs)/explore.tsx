b58d39f39eb80cb5f30f04f72bb0a7a90589e26a
Damion Morgan <only4school5680@gmail.com>
2024-08-07 18:34:49 -0400

    back to normal i think

--- app/(app)/(tabs)/explore.tsx
+++ app/(app)/(tabs)/explore.tsx
@@ -1,13 +1,11 @@
 import Ionicons from '@expo/vector-icons/Ionicons';
-import { View,Text,SafeAreaView } from 'react-native';
-import { Button, Menu, Searchbar} from 'react-native-paper';
+import { View,Text,ScrollView,SafeAreaView } from 'react-native';
+import { Button, Menu, Divider, Searchbar,Avatar, Card,} from 'react-native-paper';
 import React,{useState,useEffect} from "react"
-import { AntDesign } from '@expo/vector-icons';
+import { AntDesign,FontAwesome } from '@expo/vector-icons';
 import { getCategories,getAreas,getIngredients } from '@/utils/api/explore';
-import { fetchAllMeals, addMealToSaved, addMealToPlan,filterMeals} from '@/utils/api/explore';
-import ExploreCard from '@/components/Card';
+import ExploreCard from '@/components/ExploreCard';
 import Meal from '@/utils/interface/meal';
-
 export default function TabTwoScreen() {
   const [visibleArea, setAreaVisible] = useState(false);
   const [visibleCatergory, setCatergoryVisible] = useState(false);
@@ -100,9 +98,9 @@ export default function TabTwoScreen() {
       </View>
       {/* Body */}
       {search ? (
-        <ExploreCard fetch={fetchAllMeals} searchedQuery={searchQuery}></ExploreCard>
+        <ExploreCard searchedQuery={searchQuery}></ExploreCard>
       ) : (
-        <ExploreCard fetch={fetchAllMeals}></ExploreCard>
+        <ExploreCard></ExploreCard>
       )}
     </SafeAreaView>
   );
